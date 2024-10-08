16bfe6d1c453f6797dc8414fdc8f8b220f36c9be
Tri Cahyo <tric201@gmail.com>
2024-10-08 22:32:09 +0700

    menambah mathSin

--- fungsi2.js
+++ fungsi2.js
@@ -7,3 +7,6 @@ console.log(substract(10,5));
 const add = (a, b) => a + b;
 console.log("hasil penjumlahan 10 + 5 =");
 console.log(add(10,5));
+
+const mathSin = (number) => Math.sin(number);
+console.log(mathSin(10));
